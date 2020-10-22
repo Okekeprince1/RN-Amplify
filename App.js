c76031683d7dedd6c107ef1fde19e6f94cc60b42
Okekeprince1 <okeke.p.ebube@gmail.com>
2020-10-22 09:07:47 +0100

    designed UI

--- App.js
+++ App.js
@@ -1,26 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { createTodo } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
+
 //Aws imports
-import Amplify from 'aws-amplify'
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
 import config from './aws-exports'
 Amplify.configure(config)
 
-export default function App() {
+const initialState = { name: "", description: "" }
+
+const App = () => {
+
+  const [formState, setFormState] = useState(initialState);
+  const [todos, setTodos] = useState([]);
+
+  useEffect = (() => {
+    getTodos()
+  }, [])
+
+  const setInput = (key, value) => {
+    setFormState({ ...formState, [key]: value })
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        onChangeText={val => setInput('name', val)}
+        style={styles.input}
+        value={formState.name}
+        placeholder="Name"
+      />
+      <TextInput
+        onChangeText={val => setInput('description', val)}
+        style={styles.input}
+        value={formState.description}
+        placeholder="Description"
+      />
+      <Button title="Create Todo" onPress={addTodo} />
+      {
+        todos.map((todo, index) => (
+          <View key={todo.id ? todo.id : index} style={styles.todo}>
+            <Text style={styles.todoName}>{todo.name}</Text>
+            <Text>{todo.description}</Text>
+          </View>
+        ))
+      }
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, justifyContent: 'center', padding: 20 },
+  todo: { marginBottom: 15 },
+  input: { height: 50, backgroundColor: '#ddd', marginBottom: 10, padding: 8 },
+  todoName: { fontSize: 18 }
 });
+
+export default App;
